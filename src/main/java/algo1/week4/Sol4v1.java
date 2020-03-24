54d7b0a661b4ff0a1faa83bd3bbb358631e38a5b
Roman Kukharuk <rkukharuk@indeed.com>
2020-03-23 23:26:34 -0700

    new try

--- src/main/java/algo1/week4/Sol4v1.java
+++ src/main/java/algo1/week4/Sol4v1.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-public class Sol4 {
+public class Sol4v1 {
 
     public static BufferedWriter writer;
 
@@ -17,8 +17,7 @@ public class Sol4 {
 
     /**
      * Очередь с минимумом
-     * 38 test passed (
-     *
+     *  38 test passed ( doesnt increase speed to much
      */
     public static void main(final String[] args) throws Exception {
 
@@ -29,31 +28,50 @@ public class Sol4 {
             in = path + in;
         }
 
+        Integer min = null;
+
         writer = Files.newBufferedWriter(Paths.get(out));
         final List<String> stream = Files.readAllLines(Paths.get(in));
         final int size = Integer.parseInt(stream.get(0));
 
         final Queue<Integer> queue = new LinkedList<>();
         final PriorityQueue<Integer> pq = new PriorityQueue<>();
+        final PriorityQueue<Integer> pqNext = new PriorityQueue<>();
 
         for (int i = 1; i <= size; i++) {
             final String cmd = stream.get(i);
             if (cmd.charAt(0) == '+') {
                 final int a = Integer.parseInt(cmd.split(" ")[1]);
+                if (min == null) {
+                    min = a;
+                    pq.offer(a);
+                } else {
+                    if (a < min) {
+                        pq.offer(a);
+                        min = a;
+                    } else {
+                        pqNext.offer(a);
+                    }
+                }
                 queue.offer(a);
-                pq.offer(a);
+//                pq.offer(a);
             } else if (cmd.charAt(0) == '?') {
-                writer.write(String.valueOf(pq.peek()));
+                Integer rez = pq.peek();
+                if (rez == null) rez = pqNext.peek();
+                writer.write(String.valueOf(rez));
                 writer.newLine();
             } else { // supposed to be -
-                final int el = queue.poll();
-                pq.remove(el);
+                final Integer el = queue.poll();
+                final boolean rez = pq.remove(el);
+                if (!rez) pqNext.remove(el);
             }
 
         }
 
         writer.close();
     }
+
+
 }
 
 
